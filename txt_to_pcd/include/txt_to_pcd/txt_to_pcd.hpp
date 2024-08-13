5293770194fccfb80821f1afd01fe7ab12197380
zyicome <573332957@qq.com>
2024-08-13 15:31:41 +0800

    optimization

--- txt_to_pcd/include/txt_to_pcd/txt_to_pcd.hpp
+++ txt_to_pcd/include/txt_to_pcd/txt_to_pcd.hpp
@@ -1,21 +1,13 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include "cv_bridge/cv_bridge.h"
 #include <iostream>
 #include <fstream>
 #include <rclcpp/rclcpp.hpp>
 #include <pcl/point_cloud.h>
-#include <sensor_msgs/msg/detail/image__struct.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <sensor_msgs/msg/compressed_image.hpp>
 #include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <vector>
 
 class TxtToPcd : public rclcpp::Node
 {
